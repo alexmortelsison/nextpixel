eb172e670318f6b5f8f698c0aeb22bb51156c47f
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-20 21:02:29 +0900

    updated hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,27 +1,31 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
     <div className="max-w-7xl mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-2 place-items-center mt-24">
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <Image src={"/bg.png"} alt="bg" fill className="-z-50" />
         <div className="max-w-7xl mx-auto">
-          <h1 className="text-8xl font-extrabold w-[1000px] mx-auto tracking-tighter">
+          <h1 className="text-8xl font-extrabold w-[1000px] mx-auto tracking-tighter font-sans text-white">
             Next-Level Web Design - Built to{" "}
-            <span className="text-cyan-400">Impress</span>
+            <span className="text-[#82d8dd]">Impress.</span>
           </h1>
-          <p className="text-gray-400 text-xl mt-4 w-[600px] tracking-tight">
-            At NextPixel, we combine thoughtful design with smart technology to
-            create websites that are not only visually stunning but also highly
-            effective.
+          <p className="text-gray-200 text-xl w-[600px] tracking-tighter font-sans mt-4">
+            At <span className="text-[#82d8dd]">NextPixel</span>, we combine
+            thoughtful design with smart technology to create websites that are
+            not only visually stunning but also highly effective.
           </p>
+          <button className="bg-[#82d8dd] text-white px-8 py-2 rounded-xl mt-12 shadow-md shadow-gray-400">
+            Learn More.
+          </button>
         </div>
 
-        <video
-          src={"/hero.mp4"}
+        <Image
+          alt="hero"
+          src={"/hero.png"}
           width={1000}
           height={1000}
-          className="object-cover ml-62 w-150 h-150"
-          loop
-          muted
-          autoPlay={true}
+          className="object-cover ml-24 w-150 h-150"
         />
       </div>
     </div>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Navbar() {
-  return <nav className="max-w-7xl mx-auto py-8">Navbar</nav>;
+  return (
+    <nav className="max-w-7xl mx-auto py-4">
+      <div className="flex justify-between">
+        <div className="flex items-center">
+          <Link href={"/"}>
+            <Image
+              src={"/logo.png"}
+              alt="logo"
+              width={200}
+              height={200}
+              className="object-cover w-40 h-40"
+            />
+          </Link>
+        </div>
+        <p></p>
+      </div>
+    </nav>
+  );
 }
