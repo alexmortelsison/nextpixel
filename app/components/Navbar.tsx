eae8d6a70bd546105942b481df3796d5ec1f110e
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-24 08:02:45 +0900

    added sheet

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,18 @@
 "use client";
+import { BiMenuAltRight } from "react-icons/bi";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
 
 const navLinks = [
   {
@@ -25,7 +36,7 @@ const navLinks = [
 export default function Navbar() {
   const pathName = usePathname();
   return (
-    <nav className="max-w-7xl mx-auto lg:px-0 md:px-8">
+    <nav className="max-w-7xl mx-auto lg:px-0 md:px-8 pr-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
           <Link href={"/"}>
@@ -55,7 +66,36 @@ export default function Navbar() {
             </div>
           ))}
         </div>
-        <button>A</button>
+        <div className="lg:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <BiMenuAltRight className="text-white" size={30} />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader className="flex justify-center items-center">
+                <SheetTitle>Welcome to NextPixel</SheetTitle>
+                <Separator />
+                <SheetDescription className="mt-8">
+                  <div className="flex flex-col justify-center items-center gap-y-4">
+                    {navLinks.map((link) => (
+                      <Link
+                        key={link.href}
+                        href={link.href}
+                        className={`tracking-tight ${
+                          pathName === link.href
+                            ? "text-[#82d8dd] font-bold underline underline-offset-8"
+                            : "text-gray-400 hover:underline underline-offset-8"
+                        }`}
+                      >
+                        {link.name}
+                      </Link>
+                    ))}
+                  </div>
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
     </nav>
   );
