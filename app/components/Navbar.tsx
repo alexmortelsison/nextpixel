fa8b346e1217099b4b14dfbb0c9c9dd8e227007a
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-24 08:11:26 +0900

    changed from sheet to dropdown-menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,6 +13,21 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Separator } from "@/components/ui/separator";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const navLinks = [
   {
@@ -67,34 +82,31 @@ export default function Navbar() {
           ))}
         </div>
         <div className="lg:hidden">
-          <Sheet>
-            <SheetTrigger>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
               <BiMenuAltRight className="text-white" size={30} />
-            </SheetTrigger>
-            <SheetContent>
-              <SheetHeader className="flex justify-center items-center">
-                <SheetTitle>Welcome to NextPixel</SheetTitle>
-                <Separator />
-                <SheetDescription className="mt-8">
-                  <div className="flex flex-col justify-center items-center gap-y-4">
-                    {navLinks.map((link) => (
-                      <Link
-                        key={link.href}
-                        href={link.href}
-                        className={`tracking-tight ${
-                          pathName === link.href
-                            ? "text-[#82d8dd] font-bold underline underline-offset-8"
-                            : "text-gray-400 hover:underline underline-offset-8"
-                        }`}
-                      >
-                        {link.name}
-                      </Link>
-                    ))}
-                  </div>
-                </SheetDescription>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <Separator />
+              <DropdownMenuItem className="flex flex-col justify-center">
+                <div className="flex flex-col justify-center items-center gap-y-4">
+                  {navLinks.map((link) => (
+                    <Link
+                      key={link.href}
+                      href={link.href}
+                      className={`tracking-tight ${
+                        pathName === link.href
+                          ? "text-[#82d8dd] font-bold underline underline-offset-8"
+                          : "text-gray-400 hover:underline underline-offset-8"
+                      }`}
+                    >
+                      {link.name}
+                    </Link>
+                  ))}
+                </div>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </nav>
